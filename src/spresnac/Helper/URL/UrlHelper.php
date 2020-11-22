e691f0aefc67989f9a847c3232092fd978ee6acf
Sascha Presnac <spresnac@users.noreply.github.com>
2020-11-22 17:43:58 +0000

    Apply fixes from StyleCI

--- src/spresnac/Helper/URL/UrlHelper.php
+++ src/spresnac/Helper/URL/UrlHelper.php
@@ -27,17 +27,19 @@ class UrlHelper
                         $path_fragment->pull($key - 1);
                         $path_fragment->pull($key);
                         $path_fragment = $path_fragment->values();
+
                         return false;
                     }
                 });
-            };
+            }
             $path_fragment = $path_fragment->reject(function (string $item) {
-                return ($item === '');
+                return $item === '';
             });
             $path_string = implode('/', $path_fragment->toArray());
-            $url_token['path'] = Str::of ($path_string)->start('/');
+            $url_token['path'] = Str::of($path_string)->start('/');
             $url = $this->build_url($url_token);
         }
+
         return $url;
     }
 
@@ -47,23 +49,25 @@ class UrlHelper
      * @param array $parsed_url
      * @return string
      */
-    public function build_url(array $parsed_url) {
-        $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
-        $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
-        $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-        $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
-        $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
-        $pass     = ($user || $pass) ? "$pass@" : '';
-        $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
-        $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
-        $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
-        return $scheme .
-            $user .
-            $pass .
-            $host .
-            $port .
-            $path .
-            $query .
+    public function build_url(array $parsed_url)
+    {
+        $scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'].'://' : '';
+        $host = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+        $port = isset($parsed_url['port']) ? ':'.$parsed_url['port'] : '';
+        $user = isset($parsed_url['user']) ? $parsed_url['user'] : '';
+        $pass = isset($parsed_url['pass']) ? ':'.$parsed_url['pass'] : '';
+        $pass = ($user || $pass) ? "$pass@" : '';
+        $path = isset($parsed_url['path']) ? $parsed_url['path'] : '';
+        $query = isset($parsed_url['query']) ? '?'.$parsed_url['query'] : '';
+        $fragment = isset($parsed_url['fragment']) ? '#'.$parsed_url['fragment'] : '';
+
+        return $scheme.
+            $user.
+            $pass.
+            $host.
+            $port.
+            $path.
+            $query.
             $fragment;
     }
 }
