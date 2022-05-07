af97f5553fae0bff3b9f94a7a9c79cf0ae69d733
Sascha Presnac <sascha@presnac.de>
2022-05-07 11:54:11 +0200

    Changing to curl to bypass problems with some server

--- src/spresnac/Helper/URL/SitemapHelper.php
+++ src/spresnac/Helper/URL/SitemapHelper.php
@@ -35,7 +35,7 @@ class SitemapHelper
     public function process_input_from_url(string $input_url): Collection
     {
         try {
-            $sitemap_string = file_get_contents($input_url);
+            $sitemap_string = $this->getUrlContent($input_url);
         } catch (Exception $e) {
             return collect();
         }
@@ -45,5 +45,27 @@ class SitemapHelper
         }
 
         return $this->process_input_from_string($sitemap_string);
+    }
+    
+    protected function getUrlContent($url): string
+    {
+        $curl_handle = curl_init();
+        curl_setopt($curl_handle, CURLOPT_URL,$url);
+        curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_handle, CURLOPT_SSL_VERIFYSTATUS, false);
+        curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl_handle, CURLOPT_DEFAULT_PROTOCOL, 'https');
+        curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0');
+        curl_setopt($curl_handle, CURLOPT_ENCODING, 'gzip, deflate');
+        curl_setopt($curl_handle, CURLOPT_HTTPHEADER, [
+            'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'DNT:1',
+            'Pragma:no-cache',
+            'content-length:0'
+        ]);
+        $query = curl_exec($curl_handle);
+        curl_close($curl_handle);
+        return $query;
     }
 }
